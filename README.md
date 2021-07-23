74cee03810245e7216e56198a471b576e84cdce5
tsmweb <tiago.tsmweb@gmail.com>
2021-07-23 03:09:13 -0300

    Implementação do pacote httputil que provê métodos http utilitários.

--- README.md
+++ README.md
@@ -6,8 +6,9 @@ Helper for services written in Golang.
 * Package cerror provides customized errors that represent status for HTTP responses and specific treatments.
 * Package concurrent/executor provides an implementation of the Executor to perform background processing, limiting resource consumption when executing a collection of jobs.
 * Package concurrent/flow provides a Flow implementation to perform background processing and notify your subscribers through an Emitter.
-* Package controller implements a Controller with utility methods.
 * Package ebus implements the event bus design pattern, being an alternative to component communication while maintaining loose coupling and separation of interests principles.  
+* Package httputil provides http utility methods.
 * Package kafka is a simple wrapper for the kafka-go segmentio library, providing tools for consuming and producing events.
 * Package middleware provides settings for CORS, GZIP and JWT token validation.
 * Package util/hashutil provides utility functions to generate and validate hash.
+ 
---
+++ httputil/httputil.go
@@ -0,0 +1,97 @@
+/*
+Package httputil provides http utility methods.
+*/
+
+package httputil
+
+import (
+	"encoding/json"
+	"fmt"
+	"mime"
+	"net/http"
+	"strings"
+)
+
+const (
+	// MimeApplicationJSON represents the MimeType "application/json".
+	MimeApplicationJSON = iota
+
+	// MimeApplicationPDF represents the MimeType "application/pdf".
+	MimeApplicationPDF
+
+	// MimeApplicationOctetStream represents the MimeType "application/octet-stream".
+	MimeApplicationOctetStream
+
+	// MimeImageJPEG represents the MimeType "image/jpeg".
+	MimeImageJPEG
+
+	// MimeTextPlain represents the MimeType "text/plain".
+	MimeTextPlain
+)
+
+var mimeTypeText = map[MimeType]string{
+	MimeApplicationJSON:        "application/json",
+	MimeApplicationPDF:         "application/pdf",
+	MimeApplicationOctetStream: "application/octet-stream",
+	MimeImageJPEG:              "image/jpeg",
+	MimeTextPlain:              "text/plain",
+}
+
+// MimeType represents the mime type ("application/json", "text/plain", "image/jpeg", ...).
+type MimeType int
+
+// String return the name of the mimeType.
+func (m MimeType) String() string {
+	return mimeTypeText[m]
+}
+
+// MimeTypeText return the name of the mimeType.
+func MimeTypeText(code MimeType) string {
+	return code.String()
+}
+
+func HasContentType(r *http.Request, mimetype MimeType) bool {
+	contentType := r.Header.Get("Content-Type")
+	if contentType == "" {
+		return mimetype == MimeApplicationOctetStream
+	}
+
+	for _, v := range strings.Split(contentType, ",") {
+		t, _, err := mime.ParseMediaType(v)
+		if err != nil {
+			break
+		}
+
+		if t == mimetype.String() {
+			return true
+		}
+	}
+
+	return false
+}
+
+// Headers http header.
+type Headers map[string]string
+
+func RespondWithHeader(w http.ResponseWriter, status int, h Headers) {
+	for k, v := range h {
+		w.Header().Set(k, v)
+	}
+
+	w.WriteHeader(status)
+}
+
+func RespondWithError(w http.ResponseWriter, status int, message string) {
+	RespondWithHeader(w, status, Headers{"Content-Type": MimeTypeText(MimeApplicationJSON)})
+	w.Write([]byte(fmt.Sprintf(`{"error_message": "%s"}`, message)))
+}
+
+func RespondWithJSON(w http.ResponseWriter, status int, data interface{}) {
+	RespondWithHeader(w, status, Headers{"Content-Type": MimeTypeText(MimeApplicationJSON)})
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		RespondWithError(w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
+	} else {
+		w.Write(jsonData)
+	}
+}
